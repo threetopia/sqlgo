b42bda7a851b8a3386b92922a4d9021407a3bd9f
Tri Hartanto <tri@trihartanto.com>
2022-06-03 21:11:27 +0700

    feat: sql where any and in

--- sqlgo_test.go
+++ sqlgo_test.go
@@ -14,21 +14,23 @@ func TestOutput(t *testing.T) {
 		).SQLFrom("poi", "poi").SQLWhere(
 			SetWhere("AND", "poi", "=", 1),
 			SetWhere("AND", "poi", "=", 2),
+			SetWhere("AND", "poi", "ANY", []string{"3", "3"}),
+			SetWhere("AND", "poi", "IN", []string{"4", "5"}),
 		), "qwe"),
 		SetSelect(NewSQLGo().SQLSelect(
 			SetSelect("asd", ""),
 			SetSelect("qwe", "qwe"),
 		).SQLFrom("poi", "poi").SQLWhere(
-			SetWhere("AND", "poi", "=", 1),
+			SetWhere("AND", "poi", "=", 6),
 		), "poi"),
 	).SQLFrom(NewSQLGo().SQLSelect(
 		SetSelect("asd", ""),
 		SetSelect("qwe", "qwe"),
 	).SQLFrom("asd", "").SQLWhere(
-		SetWhere("AND", "asd", "=", "2"),
+		SetWhere("AND", "asd", "=", "7"),
 	), "asd").SQLWhere(
-		SetWhere("AND", "asd", "=", "3"),
-		SetWhere("AND", "qwe", "=", "4"),
+		SetWhere("AND", "asd", "=", "8"),
+		SetWhere("AND", "qwe", "=", "9"),
 	)
 
 	fmt.Println(sql.BuildSQL(), sql.GetParams(), sql.GetParamsCount())
--- sqlgo_where.go
+++ sqlgo_where.go
@@ -2,10 +2,16 @@ package sqlgo
 
 import "fmt"
 
+var specialOperator = map[string]string{
+	"ANY": "= ANY ",
+	"IN":  " IN ",
+}
+
 type SQLGoWhere struct {
-	values     []sqlGoWhereValues
-	params     []interface{}
-	paramCount int
+	values      []sqlGoWhereValues
+	params      []interface{}
+	paramCount  int
+	isJoinScope bool
 }
 
 type sqlGoWhereValues struct {
@@ -43,11 +49,25 @@ func (sw *SQLGoWhere) BuildSQL() string {
 		if i > 0 {
 			sql = fmt.Sprintf("%s %s ", sql, v.whereType)
 		}
+
+		operator := v.operator
+		if vo, ok := specialOperator[v.operator]; ok {
+			v.operator = vo
+		}
+
 		switch vType := v.value.(type) {
 		case *SQLGo:
 			sql = fmt.Sprintf("%s%s%s(%s)", sql, v.whereColumn, v.operator, vType.SetParamsCount(sw.GetParamsCount()).BuildSQL())
 			sw.SetParams(vType.GetParams()...)
 			sw.SetParamsCount(vType.GetParamsCount())
+		case []string:
+			sql = buildWhereSlice(sw, sql, operator, v.whereType, v, vType)
+		case []int:
+			sql = buildWhereSlice(sw, sql, operator, v.whereType, v, vType)
+		case []int64:
+			sql = buildWhereSlice(sw, sql, operator, v.whereType, v, vType)
+		case []float64:
+			sql = buildWhereSlice(sw, sql, operator, v.whereType, v, vType)
 		default:
 			sw.SetParams(vType)
 			sw.SetParamsCount(sw.GetParamsCount() + 1)
@@ -76,4 +96,34 @@ func (sw *SQLGoWhere) SetParamsCount(paramsCount int) *SQLGoWhere {
 
 func (sw *SQLGoWhere) GetParamsCount() int {
 	return sw.paramCount
+}
+
+func (sw *SQLGoWhere) setJoinScope() *SQLGoWhere {
+	sw.isJoinScope = true
+	return sw
+}
+
+func buildWhereSlice[V string | int | int64 | float32 | float64](sw *SQLGoWhere, sql string, operator string, whereType string, vWhere sqlGoWhereValues, vType []V) string {
+	if operator == "IN" {
+		sql = fmt.Sprintf("%s%s%s%s(", sql, whereType, vWhere.whereColumn, vWhere.operator)
+		for iIn, vIn := range vType {
+			delimiter := ""
+			if iIn > 0 {
+				delimiter = ","
+			}
+			if sw.isJoinScope {
+				sql = fmt.Sprintf("%s%s%x", sql, delimiter, vIn)
+			} else {
+				sw.SetParams(vIn)
+				sw.SetParamsCount(sw.GetParamsCount() + 1)
+				sql = fmt.Sprintf("%s%s$%d", sql, delimiter, sw.GetParamsCount())
+			}
+		}
+		sql = fmt.Sprintf("%s)", sql)
+	} else {
+		sw.SetParams(vType)
+		sw.SetParamsCount(sw.GetParamsCount() + 1)
+		sql = fmt.Sprintf("%s%s%s%s($%d)", sql, whereType, vWhere.whereColumn, vWhere.operator, sw.GetParamsCount())
+	}
+	return sql
 }
